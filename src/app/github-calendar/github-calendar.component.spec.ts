aad36099cf818a50a5b6b6f7d506bb2135d0b23c
Mahlatse <mhlatse@gmail.com>
2023-07-18 23:12:13 +0200

    update angluarcore and cli from 10 to 11

--- src/app/github-calendar/github-calendar.component.spec.ts
+++ src/app/github-calendar/github-calendar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GithubCalendarComponent } from './github-calendar.component';
 
@@ -6,7 +6,7 @@ describe('GithubCalendarComponent', () => {
   let component: GithubCalendarComponent;
   let fixture: ComponentFixture<GithubCalendarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GithubCalendarComponent ]
     })
